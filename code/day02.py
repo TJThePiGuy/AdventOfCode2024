6de3b0003d9718fb743ef10053b7642ece727e66
TJThePiGuy <tjomiatek@gmail.com>
2024-12-01 23:37:55 -0500

    Created day 2 files

---
+++ code/day02.py
@@ -0,0 +1,15 @@
+import regex
+from aocd.models import Puzzle
+import numpy as np
+
+puzzle = Puzzle(year=2024, day=2)
+
+def part_a():
+
+    data = puzzle.input_data
+
+def part_b():
+    pass
+
+part_a()
+# part_b()
---
+++ notes/day02.md
@@ -0,0 +1,11 @@
+# AOC 2024 Day 2
+
+## Part A
+
+## Part B
+
+## Code
+
+```python
+
+```
